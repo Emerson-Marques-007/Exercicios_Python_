821ad894619d130358ae24cbfdc26d625ebbbaf1
Emerson Marques Cardoso Dos Santos <emerso.bh2011@gmail.com>
2026-07-06 09:51:52 -0300

    Add calculations for product and cube of inputs

--- ATV_8.py
+++ ATV_8.py
@@ -2,9 +2,12 @@ numero_1 = int(input('Digite um numero Inteiro: '))
 numero_2 = int(input('Digite outro numero Inteiro: '))
 numero_r = float(input('Digite um número real: '))
 
-#produto_n1_n2 = (numero_1 * 2) * (numero_2 / 2)
+produto_n1_n2 = (numero_1 * 2) * (numero_2 / 2)
 
 soma_n1_n3 = ( 3 * numero_1 ) + numero_r
 
-#print(produto_n1_n2)
-print(soma_n1_n3)
+cubo = numero_r ** 3
+
+print(f'O produto do dobro do primeiro com a metade do segundo: {produto_n1_n2}')
+print(f'A soma do triplo do primeiro com o terceiro: {soma_n1_n3}')
+print(f'O terceiro elevado ao cubo: {cubo}')
